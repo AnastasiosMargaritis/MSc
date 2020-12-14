b7cf5c7a1d97c4603ab8ed2d618a87e30ca590f4
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-12-14 19:55:00 +0200

    Implementing Bully algorithm, Cleanup the code, Creating menu for demonstration, adding comments everywhere.

--- Cryptography/Semester Project/Exercise 8/gcd.py
+++ Cryptography/Semester Project/Exercise 8/gcd.py
@@ -1,4 +1,4 @@
-
+ 
 def fast(a, g, N):
     # Convert exponent to bits
     g = list(bin(g)[2:])
@@ -19,5 +19,4 @@ def fast(a, g, N):
 
 print(fast(2, 1234567, 12345))
 print(fast(130, 7654321, 567))
-
 
